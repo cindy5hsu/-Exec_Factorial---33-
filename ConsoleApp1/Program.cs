35866192eafa65a3bb17839e29cf0b08417103a1
Cindy <Cindy@LAPTOP-I3P14G10>
2022-10-16 22:08:39 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                int countofEmpty = 5 - i;//空白的長度
+                int countoStar = i;//星號的長度
+
+                string row = new string(' ',countofEmpty) + new string('*', countoStar);
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+              
+            for (int j = 0; j <= (5 - (i + 1)); j++)
+                {
+
+                Console.Write(" ");
+
+                }
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
